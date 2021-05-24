aa3bb52a024b2c4397e1e65b8af8ea4c80e1d049
Heidi Rytkönen <heidi.m.rytkonen@student.jyu.fi>
2021-05-24 11:04:33 +0300

    WIP: reaction plane macro

---
+++ macros-QA/CompareToReactionPlane.C
@@ -0,0 +1,29 @@
+void CompareToReactionPlane(TString sDirName)   
+{
+    TString digitfile(Form("%s/fv0digits.root", sDirName.Data()));
+    TString mcfile(Form("%s/o2sim_Kine.root", sDirName.Data()));
+
+    TFile *fin = TFile::Open(mcfile);
+    TTree *kineTree = (TTree*)fin->Get("o2sim");
+
+    std::vector<o2::MCTrack>* mctrack = nullptr;
+    auto mcbr = kineTree->GetBranch("MCTrack");
+    mcbr->SetAddress(&mctrack);
+
+    o2::dataformats::MCEventHeader* mcheader = nullptr;
+    auto hdrbr = kineTree->GetBranch("MCEventHeader");
+    hdrbr->SetAddress(&mcheader);
+
+    int nent = kineTree->GetEntries();
+    for (int ient = 0; ient < nent; ient++) {
+        
+        kineTree->GetEntry(ient);
+
+        int ntrack = 0;
+        for (auto &t : *mcheader) {
+
+            
+
+        }
+    }
+}
